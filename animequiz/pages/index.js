4805de156fd1d9bc17523a39bdcd2699a60b088f
Jean Naciel <jeansud@hotmail.com>
2021-01-28 00:17:47 -0300

    Aula 2

--- animequiz/pages/index.js
+++ animequiz/pages/index.js
@@ -1,4 +1,8 @@
+import React from 'react'
 import styled from 'styled-components'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
@@ -25,8 +29,15 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Anime Quiz - Base</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -34,7 +45,22 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={ function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input 
+                onChange={ function (infosDoEvento) {
+                  setName (infosDoEvento.target.value);
+                }}
+                placeholder="Digite seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar 
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
